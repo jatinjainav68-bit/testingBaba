f24f4b150ed1fd48aae25d7a90f5c1bd5bc02c6a
jatinjainav68-bit <jatinjainav68@gmail.com>
2025-09-08 15:37:07 +0530

    my first commit

---
+++ testingbaba24thautoMave/src/main/java/pages/LoginPage.java
@@ -0,0 +1,15 @@
+package pages;
+
+import baselibrary.BaseLibrary;
+
+public class LoginPage extends BaseLibrary {
+
+	public void  loginpage()
+	{
+		
+	}
+	public void endofTask()
+	{
+		System.out.println("automation is done");
+	}
+}
